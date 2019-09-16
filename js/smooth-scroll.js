5d45b965864332a7335462da2e2733847ce1bcf0
Łukasz Kowalczyk <48633902+AlieDev@users.noreply.github.com>
2019-09-16 19:42:52 +0200

    Update smooth-scroll.js

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,5 +1,6 @@
 var $root = $('html, body');
 $('a').click(function() {
+    event.preventDefault();
     var href = $.attr(this, 'href');
     $root.animate({
         scrollTop: $(href).offset().top
