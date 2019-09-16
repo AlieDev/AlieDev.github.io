5557cdbf6db689c10f92e3cc48362fd1c25fe30e
Łukasz Kowalczyk <48633902+AlieDev@users.noreply.github.com>
2019-09-16 19:57:36 +0200

    Update smooth-scroll.js

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,6 +1,6 @@
 var $root = $('html, body');
-$('a').click(function() {
-    console.log(event);
+$('a').click(function(event) {
+    event.stopPropagitation();
     var href = $.attr(this, 'href');
     $root.preventDefault();
     $root.animate({
