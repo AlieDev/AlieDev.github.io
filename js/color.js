7c459684d89ab4dc2a25a938c183b6ee6d96e6cd
AlieDev <jeffeplayer@gmail.com>
2019-09-28 23:22:18 +0200

    Contact hover fix/2

--- js/color.js
+++ js/color.js
@@ -8,7 +8,7 @@ function zmianaKoloru() {
 $('.randomColor').css('color', 'rgb(' + R + ',' + G +',' + B + ')');
 $('.randomBackground').css('background', 'rgb(' + R + ',' + G +',' + B + ')');
 $('.randomGradient').css('background', 'linear-gradient(white, rgb(' + R + ',' + G +',' + B + '))');
-$('.contact-col:hover').css('background', 'radial-gradient(white, rgb(' + R + ',' + G +',' + B + '))');
+$('.contact-col').hover.css('background', 'radial-gradient(white, rgb(' + R + ',' + G +',' + B + '))');
 
 // Red changing:
 if (R == 255) {
