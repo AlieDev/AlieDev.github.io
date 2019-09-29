70dc6e235545653c1a205eed78860e3725822096
AlieDev <jefferplayer@gmail.com>
2019-09-30 00:43:32 +0200

    Random Colors

--- js/color.js
+++ js/color.js
@@ -4,11 +4,14 @@ var G = Math.floor(Math.random() * 255);
 var Gmax = 0;
 var B = Math.floor(Math.random() * 255);
 var Bmax = 0;
+var hoveredObject;
+function zmianaGradientu() { 
+    $(hoveredObject).css('background', 'radial-gradient(white, rgb(' + R + ',' + G +',' + B + '))');    
+}
 function zmianaKoloru() {
 $('.randomColor').css('color', 'rgb(' + R + ',' + G +',' + B + ')');
 $('.randomBackground').css('background', 'rgb(' + R + ',' + G +',' + B + ')');
 $('.randomGradient').css('background', 'linear-gradient(white, rgb(' + R + ',' + G +',' + B + '))');
-$('.contact-col').hover($(this).css('background', 'radial-gradient(white, rgb(' + R + ',' + G +',' + B + '))'));
 // Red changing:
 if (R == 255) {
     Rmax = 1;
@@ -46,5 +49,14 @@ if (Bmax == 0) {
     B--;
 }
 }
-console.log("dziala");
-setInterval(zmianaKoloru, 20);
+var iner;
+setInterval(zmianaKoloru, 20)
+$('.contact-col').hover(
+    function () {
+    hoveredObject = $(this);
+    iner = setInterval(zmianaGradientu, 20)},
+    function () {
+    clearInterval(iner);
+    $(hoveredObject).css('background', 'white');
+    })
+    
